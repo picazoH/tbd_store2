bab084a4bb249b77064d66cd5d365e7e481041c9
2bedigital <hector@2bedigital.com>
2014-01-03 12:19:39 +0100

    Initial commit spree_i18n and spree 2-0-stable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TbdStore2::Application.config.secret_token = 'c976d5827f720154179ee670ad2f21ee11d32bae4918e326f2e664322ecd38b0b29c870b0b6f222cdfdfb9f7af8ae48eabf1fbf984977227c31aa33a3353e57b'
